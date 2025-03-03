6fd305bac109b0ad36a4a9f11fed56d446274518
aleksmn <al-mn@yandex.ru>
2025-03-03 18:59:09 +0300

    add post and tag models

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Tag
 
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("tags", "date")
+    list_display = ("title", "date")
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+    
+
+
+
