8fa1f97a12af2a0e0b0f2610ed7b4b893cf878d7
aleksmn <al-mn@yandex.ru>
2025-04-29 18:09:07 +0300

    add all-posts view

--- blog/views.py
+++ blog/views.py
@@ -14,8 +14,14 @@ def starting_page(request):
 
 
 def posts(request):
-    pass
+    all_posts = Post.objects.all().order_by("-date")
+    return render(request, "blog/all-posts.html", {
+        "posts": all_posts
+    })
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, "blog/post-detail.html", {
+        "post": post
+    })
