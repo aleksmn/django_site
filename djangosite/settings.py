423d537468965514114f9799148780f0d8150fce
aleksmn <al-mn@yandex.ru>
2025-05-02 20:36:42 +0300

    fix debug

--- djangosite/settings.py
+++ djangosite/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', default=False) == 'True'
 
 ALLOWED_HOSTS = [os.environ.get('HOST')]
 
